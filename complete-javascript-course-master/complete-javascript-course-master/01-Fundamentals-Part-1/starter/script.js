fed0d0e8eb965dc838689ecb35ee6691b7aaa0c2
Priyanka Naidu <priyanka.naidu@venturepact.com>
2023-03-07 13:50:45 +0530

    javascript material

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,54 @@
+// let js = "Priyanka";
+// if(js == "priyanka") alert("HEllo world")
+
+console.log("Hello world");
+
+
+// const currentYear = 2023;
+// const pri = currentYear - 2000;
+// const jai = currentYear - 2008;
+// console.log(pri, jai, "Ahhh!! they are becoming old!!!");
+// // Minus operator
+//  const a = 10;
+// console.log(a + 5, a * 2, a / 2);
+
+// // Addition, Multiply and Divide operator
+
+// console.log(2**4);
+// // 2 ** 4 means 2  to the power of 4 = 2 * 2* 2 *2 
+
+
+// const firstName = "Priyanka";
+// const lastName = "Naidu";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;  //15
+// x += 10; //x = x + 10
+// x *= 4; //x =  x * 4
+// x++;
+// x--;
+// console.log(x);
+
+
+// // Assignment operators
+// // > , < , >= , <=
+// console.log(pri > jai);
+// console.log(pri < jai);
+// console.log(pri >= jai);
+// console.log(pri <= jai);
+// console.log(pri == jai);
+
+// const currentYear = 2023;
+// const pri = currentYear - 2000;
+// const jai = currentYear - 2008;
+
+// // console.log(currentYear -  );
+
+// console.log(25 - 10 - 5);
+
+// let x,y;
+// x = y =  25 - 10 - 5;
+// console.log(x,y);
+
+
+

ba01bd78cb9abd507e897304e84b8e8c41fd4f07
Priyanka Naidu <priyanka.naidu@venturepact.com>
2023-03-13 15:41:02 +0530

    [Changes in project]

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -36,5 +36,112 @@
 
 // console.log(null);
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
+// console.log(age);
+
+
+
+// var job = "programmer";
+// job = "teacher";
+
+/*
+//Mathematical operators
+const now = 2023
+const agePri = now - 2000;
+const ageAni = now - 1999;
+console.log(agePri,ageAni);
+
+console.log(ageAni * 2, ageAni / 10, 2**3);
+
+const firstName = "Priyanka";
+const lastName = "Naidu";
+console.log(firstName + ' '+lastName);
+
+
+
+// assignment operators
+let x = 10 + 5;
+console.log(x);
+x += 10; // x = x + 10 = 25;
+console.log(x);
+x *= 4; // x = x * 4 = 100;
+console.log(x);
+x++; // x = x * 4 = 100;
+console.log(x); 
+x--;
+console.log(x); 
+
+// comparison operator
+console.log(ageAni > agePri);
+console.log( agePri >= 24);
+
+const isFullAge = ageAni >= 26;
+console.log(isFullAge);
+
+console.log (now - 1991 >= now - 2018);
+*/
+// const massMarks = 78;
+// const massJohn = 92;
+// const heightMarks = 1.69;
+// const heightJohn = 1.88;
+
+// const massMarks = 95;
+// const massJohn = 85;
+// const heightMarks = 1.88;
+// const heightJohn = 1.76;
+
+// const BMIMarks = massMarks / (heightMarks * heightMarks);
+// console.log(BMIMarks);
+
+// const BMIJohn = massJohn / heightJohn ** 2;
+// console.log(BMIJohn);
+
+// const markHigherBMI = BMIMarks > BMIJohn;
+// console.log(markHigherBMI);
+
+
+// const firstName = 'Priyanka';
+// const job = 'software Developer';
+// const Birthyear = 1999;
+// const currentYear = 2023;
+
+// const sentence = "I'm " + firstName + ', a ' + (currentYear - Birthyear) + " years old " + job + "!" ;
+// console.log(sentence);
+
+// const sentenceNew = `I'm ${firstName}, a ${currentYear - Birthyear} years old ${job}!`;
+// console.log(sentenceNew);
+
+const age = 15;
+const yearsleft = 18 - age;
+
+if(age >= 18){
+    console.log("Pri is ready to ride! 🚵");
+}else{
+    console.log(`no you're not ready you are still ${age}, you have to wait ${yearsleft} years.`)
+}
+
+
+const birthyear = 1991;
+let century;
+
+if(birthyear <= 2000){
+     century = 20;
+} else {
+     century = 21;
+}
+console.log(century);
+
+
+
+
+
+
+
+
+
+
+
+
+
+

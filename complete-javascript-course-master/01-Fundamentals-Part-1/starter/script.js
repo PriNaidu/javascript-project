477da3a6b30754bd3371af7ce6fd5af181bdd3b9
Priyanka Naidu <priyanka.naidu@venturepact.com>
2023-03-13 11:11:06 +0530

    [Js data types]

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,40 @@
-let js= 'amazing';
-if (js == 'amazing') alert('js is FUN!');
-console.log(40+8+23-10);
+// let js= 'amazing';
+// console.log(40+8+23-10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = "Priyanka";
+// console.log(firstName);
+// let lastName = "Naidu";
+// console.log(firstName + lastName);
+
+// let country = "India";
+// let continent = "Asia";
+// let population = "300000";
+
+// console.log(country,continent,population);
+
+
+// let js = true;
+// console.log(js);
+
+// console.log(typeof js);
+
+// js =23;
+// console.log(typeof js);
+
+
+// let yr;
+// console.log(yr);
+// console.log(typeof yr);
+
+
+// yr = 1999;
+// console.log(yr)
+// console.log(typeof yr);
+
+// console.log(null);
+
+let age = 30;
+age = 31;
